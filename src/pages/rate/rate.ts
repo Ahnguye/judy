3029b9944897392961f0ab4875b3daf65e0eac3e
Andre Nguyen <ahnguye@ucdavis.edu>
2018-01-27 20:28:17 -0800

    stars, review, and name

--- src/pages/rate/rate.ts
+++ src/pages/rate/rate.ts
@@ -14,12 +14,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'rate.html',
 })
 export class RatePage {
+  review = {}
+  name: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.name = 'David Kim';
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RatePage');
   }
 
+  leaveRating(id, num) {
+    console.log(id); //user id
+    console.log(num); // the rating
+  }
+
+  logForm() {
+    console.log(this.review);
+  }
 }
